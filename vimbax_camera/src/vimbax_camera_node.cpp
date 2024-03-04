3ae16330ef53596d81daf542bacfac9605030a53
Karsten Höpke <karsten.hoepke@alliedvision.com>
2024-03-04 10:11:31 +0100

    Changed str_id type

--- vimbax_camera/src/vimbax_camera_node.cpp
+++ vimbax_camera/src/vimbax_camera_node.cpp
@@ -429,7 +429,9 @@ void VimbaXCameraNode::on_camera_discovery_callback(const VmbHandle_t handle, co
     handle, SFNCFeatures::EventCameraDiscoveryCameraID.data(), nullptr, count, &count);
 
   if (err == VmbErrorSuccess && count > 0) {
-    std::vector<char> str_id(count);
+    std::string str_id;
+    str_id.resize(count);
+  //  std::vector<char> str_id(count);
     err = api_->FeatureStringGet(
       handle, SFNCFeatures::EventCameraDiscoveryCameraID.data(), &str_id[0], count, &count);
     if (err == VmbErrorSuccess) {
