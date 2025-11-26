d9897346a0963b5a6b1d121a693376eba3fe9b27
Dennis Langenkamp <dennis.langenkamp@alliedvision.com>
2025-11-26 10:24:08 +0000

    Fix handling of missing features
    Some legacy cameras like the Mako do not support all SFNC features. In the "camera_info_get" function these missing feature were not handled correctly and the function was always failing, if one feature could not be accessed. This is fixed by checking the return code for VmbErrorNotFound.

--- vimbax_camera/src/vimbax_camera.cpp
+++ vimbax_camera/src/vimbax_camera.cpp
@@ -749,7 +749,9 @@ result<double> VimbaXCamera::feature_float_get(
   auto const err =
     api_->FeatureFloatGet(handle, name.data(), reinterpret_cast<double *>(&value));
 
-  if (err != VmbErrorSuccess) {
+  if (err == VmbErrorNotFound) {
+    return error{err};
+  } else if (err != VmbErrorSuccess) {
     RCLCPP_ERROR(
       get_logger(), "%s failed with error %d (%s)", __FUNCTION__, err,
       vmb_error_to_string(err).data());
@@ -1374,17 +1376,28 @@ result<VimbaXCamera::Info> VimbaXCamera::camera_info_get() const
 
   auto const firmware_version = feature_string_get(SFNCFeatures::DeviceFirmwareVersion);
   if (!firmware_version) {
-    return firmware_version.error();
+    if (firmware_version.error().code != VmbErrorNotFound) {
+      return firmware_version.error();
+    } else {
+      info.firmware_version = "N/A";
+    }
+  } else {
+    info.firmware_version = *firmware_version;
   }
-  info.firmware_version = *firmware_version;
+  
 
   info.device_id = camera_info_.cameraIdString;
 
   auto const device_user_id = feature_string_get(SFNCFeatures::DeviceUserId);
   if (!device_user_id) {
-    return device_user_id.error();
+    if (device_user_id.error().code != VmbErrorNotFound) {
+      return device_user_id.error();
+    } else {
+      info.device_user_id = "N/A";
+    }
+  } else {
+    info.device_user_id = *device_user_id;
   }
-  info.device_user_id = *device_user_id;
 
   info.device_serial_number = camera_info_.serialString;
 
@@ -1420,8 +1433,10 @@ result<VimbaXCamera::Info> VimbaXCamera::camera_info_get() const
 
   auto const frame_rate = feature_float_get(SFNCFeatures::AcquisitionFrameRate);
   if (!frame_rate) {
-    if (frame_rate.error().code != VmbErrorNotAvailable) {
+    if (frame_rate.error().code != VmbErrorNotFound) {
       return frame_rate.error();
+    } else {
+      info.frame_rate = 0.0;
     }
   } else {
     info.frame_rate = *frame_rate;
